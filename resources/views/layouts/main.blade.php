02c43218be7b8169eff9e4f4ed7eb8164e6dd07a
Kevin <69651491+Kevin-Ponciano@users.noreply.github.com>
2022-05-10 11:14:55 -0300

    Implementando Layouts
    Substituindo Templates por layouts

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,12 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ODONTO ON</title>
+    <title>@yield('title')</title>
     <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/checkout/">
 
     <!-- Bootstrap core CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-  
+
 
   <style>
     .bd-placeholder-img {
@@ -30,9 +30,14 @@
 
   <!-- Custom styles for this template -->
   <link href="form-validation.css" rel="stylesheet">
-  
+
 
 </head>
-    <!-- O arquivo que utilizara este template precisa usar o nome content em sua section-->
-    @yield('content')
+    <body class="text-center">
+        <!-- O arquivo que utilizara este template precisa usar o nome content em sua section-->
+        @yield('content')
+        <footer>
+            <p class="mt-5 mb-3 text-muted"><br/>&copy; 2022 Odonto On</p>
+        </footer>
+    </body>
 </html>
