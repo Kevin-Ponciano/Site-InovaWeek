a5a47b8b3c9c6423262662d93db90fed1c814c7f
Kevin <69651491+Kevin-Ponciano@users.noreply.github.com>
2022-05-12 09:59:43 -0300

    update Jetstream Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -31,14 +31,16 @@
                 </label>
             </div>
 
-            <div class="flex items-center justify-end mt-4">
+            <div class="flex justify-end items-center mt-4">
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                    <a class="flex-1 justify-start underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
-
-                <x-jet-button class="ml-4">
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
+                        {{ __('Não Registrado?') }}
+                    </a>
+                <x-jet-button class="ml-4 ">
                     {{ __('Log in') }}
                 </x-jet-button>
             </div>
