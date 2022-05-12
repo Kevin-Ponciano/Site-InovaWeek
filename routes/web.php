880e6d888f7dab65dbe2cb7082768d3dd1dea0fc
Kevin <69651491+Kevin-Ponciano@users.noreply.github.com>
2022-05-12 12:54:26 -0300

    update

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,9 @@ use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', [LoginController::class, 'index']);
+//Route::get('/login', [LoginController::class, 'index']);
 
-Route::get('/cadastro', [CadastroController::class, 'index']);
+//Route::get('/cadastro', [CadastroController::class, 'index']);
 
 Route::post('/views',[CadastroController::class, 'store']);
 
