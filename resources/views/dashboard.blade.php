63aea563343b489bf885ca13edbb37882d991e5f
Kevin-Ponciano <kevinmonteiro2014@live.com>
2022-05-24 20:23:24 -0300

    update calendar

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,10 +9,6 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <x-readme/>
-
-{{--            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">--}}
-{{--                <x-jet-welcome />--}}
-
             </div>
         </div>
 </x-app-layout>
