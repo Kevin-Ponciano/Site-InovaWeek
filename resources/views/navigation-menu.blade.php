20ae60acccdda94f74462a9a43e58c12dfd925e2
Kevin <69651491+Kevin-Ponciano@users.noreply.github.com>
2022-05-24 16:43:28 -0300

    Update navigation-menu.blade.php

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -31,6 +31,11 @@
                         {{'Registro de Doenças'}}
                     </x-jet-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-jet-nav-link href="{{route('evento')}}" :active="request()->routeIs('evento')">
+                        {{'Calendário'}}
+                    </x-jet-nav-link>
+                </div>
             </div>
 
             <div class="hidden sm:flex sm:items-center sm:ml-6">
