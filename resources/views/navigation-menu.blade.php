feb322bc1953a2d1935301441b4a64069ea55849
Garchy <byel20102010@gmail.com>
2022-05-25 18:35:56 -0300

    arrumando tela de busca

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -26,11 +26,6 @@
                         {{ __('Perfil') }}
                     </x-jet-nav-link>
                 </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-jet-nav-link href="{{route('doencas')}}" :active="request()->routeIs('doencas')">
-                        {{'Registro de Doenças'}}
-                    </x-jet-nav-link>
-                </div>
             </div>
 
             <div class="hidden sm:flex sm:items-center sm:ml-6">
