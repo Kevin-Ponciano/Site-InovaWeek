2331aa5184848fa0c6e17e01f71a68bd95cc9eec
Kevin <69651491+Kevin-Ponciano@users.noreply.github.com>
2022-09-20 00:25:59 -0300

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,12 +60,12 @@
 <body class="font-sans no-underline">
 <x-jet-banner/>
 
-<div class="bg-gray-100">
+<div class="bg-gray-100" style="height: border-box">
     @livewire('navigation-menu')
 
     <!-- Page Heading -->
     @if (isset($header))
-        <header class="bg-white shadow">
+        <header class="bg-white shadow ">
             <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                 {{ $header }}
             </div>
@@ -76,6 +76,7 @@
     <!-- Page Content -->
     <main>
         {{ $slot }}
+        <br>
     </main>
 
 
